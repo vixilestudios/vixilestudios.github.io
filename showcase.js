88637930bbb66142d653b48b1434eae7894c7e4f
Yogurt Gaming <Xeraver123@gmail.com>
2024-05-05 21:37:32 +0200

    slide indicator

--- showcase.js
+++ showcase.js
@@ -16,11 +16,24 @@ const texts = [
 let index = 0;
 
 function changeContent() {
-  const { explanation, showcase, explanationText, showcaseText } = texts[index % texts.length];
+  const currentIndex = index % texts.length;
+
+  const prevIndicatorBar = document.querySelector('.selected-bar');
+  if (prevIndicatorBar) {
+    prevIndicatorBar.classList.remove('selected-bar');
+    prevIndicatorBar.classList.add('bottom-bar');
+  }
+
+  const { explanation, explanationText } = texts[currentIndex];
   document.querySelector('.explanation h1').textContent = explanation;
   document.querySelector('.explanation .h').textContent = explanationText;
-  document.querySelector('.showcase img').src = images[index % images.length];
+  document.querySelector('.showcase img').src = images[currentIndex];
+
+  const indicatorBars = document.querySelectorAll('.bottom-bars .bottom-bar');
+  indicatorBars[currentIndex].classList.remove('bottom-bar');
+  indicatorBars[currentIndex].classList.add('selected-bar');
+
   index++;
 }
 
-setInterval(changeContent, 5000);
+setInterval(changeContent, 8000);
